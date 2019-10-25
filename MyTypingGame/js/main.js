b1947a9962065b31dcd21a96815298cad74853e7
Takashi <Takashi@gmail.com>
2019-10-25 20:55:40 +0900

    時間設定を行った

--- MyTypingGame/js/main.js
+++ MyTypingGame/js/main.js
@@ -1,19 +1,68 @@
-'use strict'
+'use strict';
 
 {
-  const word = 'apple';
+  const words = [
+    'apple',
+    'sky',
+    'blue',
+    'middle',
+    'set',
+  ];
+
+  let word = words[Math.floor(Math.random() * words.length)];
   let loc = 0;
+  let score = 0;
+  let miss = 0;
+  const timeLimit = 3 * 1000;
+  let startTime;
 
   const target = document.getElementById('target');
-  target.textContent = word;
+  const scoreLabel = document.getElementById('score');
+  const missLabel = document.getElementById('miss');
+  const timerLabel = document.getElementById('timer');
+
+  function updateTarget() {
+    let placeholder = '';
+    for (let i = 0; i < loc; i++) {
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+
+  function updateTimer() {
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+
+    const timeoutId = setTimeout(() => {
+      updateTimer();
+    }, 10);
+
+    if (timeLeft < 0) {
+      clearTimeout(timeoutId);
+      alert('game over');
+    }
+  }
+  
+
+  window.addEventListener('click', () => {
+    target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
+  });
 
   window.addEventListener('keydown', e => {
-    console.log(e.key);
     if (e.key === word[loc]) {
-      console.log('score');
       loc++;
+      if (loc === word.length) {
+        word = words[Math.floor(Math.random() * words.length)];
+        loc = 0;
+      }
+      updateTarget();
+      score++;
+      scoreLabel.textContent = score;
     } else {
-      console.log('miss');
+      miss++;
+      missLabel.textContent = miss;
     }
   });
 }
