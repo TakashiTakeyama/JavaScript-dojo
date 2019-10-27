779b3438a7e8d8a2df5d385d558f5413152b1d2f
Takashi <Takashi@gmail.com>
2019-10-27 21:53:02 +0900

    作業途中

---
+++ MyNumbersGame/js/main.js
@@ -0,0 +1,85 @@
+'use strict';
+
+{
+  class Panel {
+    constructor() {
+      this.el = document.createElement('li');
+      this.el.classList.add('pressed');
+      this.el.addEventListener('click', () => {
+        this.check();
+      });
+    }
+
+    getEl() {
+      return this.el;
+    }
+
+    activate(num) {
+      this.el.classList.remove('pressed');
+      this.el.textContent = num;
+    }
+
+    check() {
+      //this.el.textContentは文字列なので、整数に直してあげる。以下の場合10進数
+      if (currentNum === parseInt(this.el.textContent, 10)) {
+        this.el.classList.add('pressed');
+        currentNum++;
+
+        if (currentNum === 4) {
+          clearTimeout(timeoutId);
+        }
+      }
+    }
+  }
+
+  class Board {
+    constructor() {
+      this.panels = [];
+      for (let i = 0; i < 4; i++) {
+        this.panels.push(new Panel());
+      }
+      this.setup();
+    }
+
+    setup() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        /*クラスのプロパティに外部から直接アクセスしない方が良い
+        board.appendChild(panel.el); */
+        board.appendChild(panel.getEl());// カプセル化
+      })
+    }
+
+    activate() {
+      const nums = [0, 1, 2, 3];
+
+      this.panels.forEach(panel => {
+        const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
+        panel.activate(num);
+      })
+    }
+  }
+
+  function runTimer() {
+    const timer = document.getElementById('timer');
+    timer.textContent = ((Date.now() - startTime) / 1000).toFixed(2);
+
+    timeoutId = setTimeout(() => {
+      runTimer();
+    }, 10);
+  }
+
+  const board = new Board;
+
+  let currentNum = 0;
+  let startTime;
+  let timeoutId;
+
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', () => {
+    board.activate();
+
+    startTime = Date.now();
+    runTimer();
+  });
+}
