bf9cf3bb31e8f18fcda2107eaf857bf7ed13a650
Takashi Takeyama <chongzhizhushan74@gmail.com>
2019-11-08 22:23:39 +0900

    コードレシピ集を始める

---
+++ udemy/script.js
@@ -0,0 +1,54 @@
+/*
+
+*/
+// console.log('Hello World');
+
+// var firstName = 'John';
+// console.log(firstName);
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullage = true;
+// console.log(fullage);
+
+// var job;
+// console.log(job);
+
+// job = 'Teacher';
+// console.log(job);
+
+// Variable naming rules;
+// var _3years = 3;
+// var Johnmark = 'John and Mark';
+// var if = 23;
+
+// console.log(firstName + ' ' + age);
+
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = false;
+
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married' + isMarried);
+// age = 'twenty eight';
+// job = 'driver';
+
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married' + isMarried);
+
+// prompt('What is this his last Name?');
+// console.log(firstName + ' ' + lastName);
+
+// var year, yearJohn, yearMark;
+// var now = 2020;
+// var yearJohn = now - 28;
+// var yearMark = now - 33;
+
+// console.log(yearJohn);
+
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
+
+// var num1 = 255;
+// console.log(num1.toString(16));
+// console.log(num1.toString(8));
